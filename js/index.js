df83ec99591ac69451052ebf2bb4436a88d06542
SaadiaElf <saadiaelfekak@gmail.com>
2022-09-27 13:13:24 +0100

    Adding video controls

--- js/index.js
+++ js/index.js
@@ -1,4 +1,39 @@
-let exerciseCards = document.querySelectorAll(".e__card");
-let randomIndex = Math.floor(Math.random() * exerciseCards.length);
+function videoControls() {
+  let startBtn = document.querySelector(".btn--play");
+  let pauseBtn = document.querySelector(".btn--pause");
+  let stopBtn = document.querySelector(".btn--stop");
+  let video = document.querySelector(".e__card__video");
 
-exerciseCards[randomIndex].classList.remove("e__card--hidden");
+  startBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    video.contentWindow.postMessage(
+      '{"event":"command","func":"' + "playVideo" + '","args":""}',
+      "*"
+    );
+  });
+
+    stopBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+    video.contentWindow.postMessage(
+      '{"event":"command","func":"' + "stopVideo" + '","args":""}',
+      "*"
+    );
+  });
+
+    pauseBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+    video.contentWindow.postMessage(
+      '{"event":"command","func":"' + "pauseVideo" + '","args":""}',
+      "*"
+    );
+  });
+}
+
+function getRandomCard() {
+  let exerciseCards = document.querySelectorAll(".e__card");
+  let randomIndex = Math.floor(Math.random() * exerciseCards.length);
+
+  exerciseCards[randomIndex].classList.remove("e__card--hidden");
+}
+getRandomCard();
+videoControls();
