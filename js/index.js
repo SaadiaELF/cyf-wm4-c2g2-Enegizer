e66072f48a0854106fb499557027c9bf76e9b31c
Lunamzelo <lunamzelo@gmail.com>
2022-09-29 23:56:30 +0100

    added timer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+let startBtn = document.querySelectorAll(".btn--play");
+let pauseBtn = document.querySelectorAll(".btn--pause");
+let stopBtn = document.querySelectorAll(".btn--stop");
+let videos = document.querySelectorAll(".e__card__video");
+
+// Hide menu on mobile view after clicking on menu items
 function hideMenu() {
   let menuOpen = document.querySelector("#h__side-menu").checked;
 
@@ -5,15 +11,9 @@ function hideMenu() {
     document.querySelector("#h__side-menu").checked = false;
   }
 }
-window.addEventListener("scroll", hideMenu);
 
 // Functionalities of play/pause/stop buttons
 function videoControls() {
-  let startBtn = document.querySelectorAll(".btn--play");
-  let pauseBtn = document.querySelectorAll(".btn--pause");
-  let stopBtn = document.querySelectorAll(".btn--stop");
-  let videos = document.querySelectorAll(".e__card__video");
-
   // Looping trough all the start buttons and applying the click event
   for (let i = 0; i < videos.length; i++) {
     startBtn[i].addEventListener("click", function () {
@@ -21,6 +21,7 @@ function videoControls() {
         '{"event":"command","func":"' + "playVideo" + '","args":""}',
         "*"
       );
+      timer();
     });
   }
 
@@ -44,6 +45,7 @@ function videoControls() {
     });
   }
 }
+
 // Generate random card/exercise in every load
 function getRandomCard() {
   let exerciseCards = document.querySelectorAll(".e__card");
@@ -51,7 +53,29 @@ function getRandomCard() {
 
   exerciseCards[randomIndex].classList.remove("e__card--hidden");
 }
+
+//  Timer
+let now = new Date().getTime();
+let countDownTime = new Date(now + 15 * 60 * 1000);
+
+function timer() {
+  setInterval(function () {
+    let currentTime = new Date().getTime();
+    let timeLeft = countDownTime - currentTime;
+
+    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.querySelector("#timer").innerHTML = `${minutes}:${seconds}`;
+
+    if (timeLeft < 0) {
+      clearInterval(timer);
+      document.querySelector("#timer").innerHTML = "00:00";
+    }
+  }, 1000);
+}
+
+// Calling functions
+window.addEventListener("scroll", hideMenu);
 getRandomCard();
 videoControls();
-
-
